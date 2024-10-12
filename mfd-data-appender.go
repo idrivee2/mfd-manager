29ba813d1f211a59f330de54468596f1dd54901d
Muhammad Abrar <abrar1984@gmail.com>
2024-10-12 08:55:50 +0500

    First commit

--- mfd-data-appender.go
+++ mfd-data-appender.go
@@ -291,7 +291,7 @@ func (l *MfdDataAppender) creatingTargetPathReliable() error {
 		if err := os.MkdirAll(l.diskPath, 0755); err == nil {
 			break
 		} else if i > 20 {
-			log.Fatal("creatingTargetPathReliable failed|", err)
+			log.Println("creatingTargetPathReliable failed|", err)
 			return err
 		}
 		time.Sleep(10 * time.Millisecond)
